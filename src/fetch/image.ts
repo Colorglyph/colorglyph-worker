a28d5041712e7c77f34dbdc6ee87e2c8050e8791
Tyler van der Hoeven <hi@tyvdh.com>
2023-10-18 15:36:29 -0400

    implement cache for images

--- src/fetch/image.ts
+++ src/fetch/image.ts
@@ -6,10 +6,21 @@ export async function image(req: IRequestStrict, env: Env, ctx: ExecutionContext
     if (!object)
         return status(404)
 
-    return new Response(object.body, {
+    const cache = caches.default
+    const match = await cache.match(req)
+
+    if (match)
+        return match
+
+    const res = new Response(object.body, {
         headers: {
+            'Cache-Control': 'public, max-age=2419000, immutable',
             'Content-Type': 'image/png',
             'Content-Length': object.size.toString(),
         }
     })
+
+    ctx.waitUntil(cache.put(req.clone(), res.clone()))
+
+    return res
 }
